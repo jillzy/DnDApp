9018737bd637416ccb85d3dc67c5e4bd7e502e03
Jill <jillzyeung@gmail.com>
2015-07-11 23:53:15 -0700

    Initial commit to repository

---
+++ Assets/scripts/components/Stats.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats : MonoBehaviour {
+
+	public int str, dex, con, inte, wis, cha;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetStr (int str) {
+		this.str = str;
+	}
+
+	public void SetDex (int dex) {
+		this.dex = dex;
+	}
+
+	public void SetCon (int con) {
+		this.con = con;
+	}
+
+	public void SetInte (int inte) {
+		this.inte = inte;
+	}
+
+	public void SetWis (int wis) {
+		this.wis = wis;
+	}
+
+	public void SetCha (int cha) {
+		this.cha = cha;
+	}
+
+	public int Roll() {
+		return Random.Range (1, 7);
+	}
+
+	public int RollStr () {
+		return (Roll() + this.str);
+	}
+
+	public int RollDex () {
+		return (Roll() + this.dex);
+	}
+
+	public int RollCon () {
+		return (Roll() + this.con);
+	}
+
+	public int RollInte () {
+		return (Roll() + this.inte);
+	}
+
+	public int RollWis () {
+		return (Roll() + this.wis);
+	}
+
+	public int RollCha () {
+		return (Roll() + this.cha);
+	}
+
+	void OnGUI(){
+	//	GUI.Label(Rect(0,0,100,50),"STR: " + str + "\n");
+		GUI.Label(new Rect(0,0,Screen.width,Screen.height), "STR: " + str);
+	}
+
+}
---
+++ Assets/scripts/components/Traits.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Traits : MonoBehaviour {
+
+	public string name, charClass;
+	public char sex;
+	public int age, height, weight, feet, inches;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void SetName (string name) {
+		this.name = name;
+	}
+	
+	public void SetClass (string charClass) { // additional attributes or consequences of class?
+		this.charClass = charClass;
+	}
+	
+	public void SetSex (char sex) {
+		this.sex = sex;
+	}
+	
+	public void SetAge (int age) {
+		this.age = age;
+	}
+
+	public void SetWeight (int weight) {
+		this.weight = weight;
+	}
+
+	public void SetHeight (int feet, int inches){
+		this.feet = feet;
+		this.inches = inches;
+		this.height = ((feet * 60) + inches);
+	}
+
+
+	
+
+}
