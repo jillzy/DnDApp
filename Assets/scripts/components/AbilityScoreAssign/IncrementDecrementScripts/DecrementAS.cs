73ff65b42b1d6b1278c42ba4fcf2eb0c664eaca6
Jill <jillzyeung@gmail.com>
2015-07-28 00:19:33 -0700

    Working prototype of abilty score UI

--- Assets/scripts/components/AbilityScoreAssign/IncrementDecrementScripts/DecrementAS.cs
+++ Assets/scripts/components/AbilityScoreAssign/IncrementDecrementScripts/DecrementAS.cs
@@ -6,46 +6,46 @@ public class DecrementAS : MonoBehaviour {
 	int newValue = 0;
 	
 	public void DecrementScore(int ability) {
-
+		
 		switch (ability) {
-			case 1: 
-			if (GameInformation.PlayerOneAS.str == 0) { 
-					ASUIUpdater.UpdateScoreAndText (1, 18);
-				} else {
-					newValue = GameInformation.PlayerOneAS.str - 1;
-					ASUIUpdater.UpdateScoreAndText (1, newValue);
-				}
-				break;
+		case 1: 
+			newValue = (int) Mod(GameInformation.PlayerOneAS.str - 1, 19);
+			ASUIUpdater.UpdateScoreAndText (1, newValue);
+			break;
 			
-			case 2:
-				newValue = GameInformation.PlayerOneAS.dex - 1;
-				ASUIUpdater.UpdateScoreAndText (2, newValue);
-				break;
+		case 2:
+			newValue = (int) Mod(GameInformation.PlayerOneAS.dex - 1, 19);
+			ASUIUpdater.UpdateScoreAndText (2, newValue);
+			break;
 			
 			
-			case 3:
-				newValue = GameInformation.PlayerOneAS.con - 1;
-				ASUIUpdater.UpdateScoreAndText (3, newValue);
-				break;
+		case 3:
+			newValue = (int) Mod(GameInformation.PlayerOneAS.con - 1, 19);
+			ASUIUpdater.UpdateScoreAndText (3, newValue);
+			break;
 			
 			
-			case 4:
-				newValue = GameInformation.PlayerOneAS.inte - 1;
-				ASUIUpdater.UpdateScoreAndText (4, newValue);
-				break;
-				
-			case 5:
-				newValue = GameInformation.PlayerOneAS.wis - 1;
-				ASUIUpdater.UpdateScoreAndText (5, newValue);
-				break;
+		case 4:
+			newValue = (int) Mod(GameInformation.PlayerOneAS.inte - 1, 19);
+			ASUIUpdater.UpdateScoreAndText (4, newValue);
+			break;
 			
-			case 6:
-				newValue = GameInformation.PlayerOneAS.cha - 1;
-				ASUIUpdater.UpdateScoreAndText (6, newValue);
-				break;	
+		case 5:
+			newValue = (int) Mod(GameInformation.PlayerOneAS.wis - 1, 19);
+			ASUIUpdater.UpdateScoreAndText (5, newValue);
+			break;
 			
-			default: 
-				break;	
+		case 6:
+			newValue = (int) Mod(GameInformation.PlayerOneAS.cha - 1, 19);
+			ASUIUpdater.UpdateScoreAndText (6, newValue);
+			break;	
+			
+		default: 
+			break;	
 		}
+	}
+	
+	float Mod(float a, float b) {
+		return a - b * Mathf.Floor (a / b);
 	}
 }
--- Assets/scripts/components/AbilityScoreAssign/IncrementDecrementScripts/IncrementAS.cs
+++ Assets/scripts/components/AbilityScoreAssign/IncrementDecrementScripts/IncrementAS.cs
@@ -2,52 +2,48 @@
 using System.Collections;
 
 public class IncrementAS : MonoBehaviour {
-
+	
 	int newValue = 0;
 	
-
+	
 	public void IncrementScore(int ability) {
-
+		
 		switch (ability) {
-		
-			case 1: 
-				if (GameInformation.PlayerOneAS.str == 18) { 
-					ASUIUpdater.UpdateScoreAndText (1, 0);
-				} else {
-					newValue = GameInformation.PlayerOneAS.str + 1;
-					ASUIUpdater.UpdateScoreAndText (1, newValue);
-				}
-				break;
-		
-			case 2:
-				newValue = GameInformation.PlayerOneAS.dex + 1;
-				ASUIUpdater.UpdateScoreAndText (2, newValue);
-				break;
-				
-		
-			case 3:
-				newValue = GameInformation.PlayerOneAS.con + 1;
-				ASUIUpdater.UpdateScoreAndText (3, newValue);
-				break;
-				
-
-			case 4:
-				newValue = GameInformation.PlayerOneAS.inte + 1;
-				ASUIUpdater.UpdateScoreAndText (4, newValue);
-				break;
-				
-			case 5:
-				newValue = GameInformation.PlayerOneAS.wis + 1;
-				ASUIUpdater.UpdateScoreAndText (5, newValue);
-				break;
-				
-			case 6:
-				newValue = GameInformation.PlayerOneAS.cha + 1;
-				ASUIUpdater.UpdateScoreAndText (6, newValue);
-				break;	
-				
-			default: 
-				break;	
+			
+		case 1: 
+			newValue = (GameInformation.PlayerOneAS.str + 1) % 19;
+			ASUIUpdater.UpdateScoreAndText (1, newValue);
+			break;
+			
+		case 2:
+			newValue = (GameInformation.PlayerOneAS.dex + 1) % 19;
+			ASUIUpdater.UpdateScoreAndText (2, newValue);
+			break;
+			
+			
+		case 3:
+			newValue = (GameInformation.PlayerOneAS.con + 1) % 19;
+			ASUIUpdater.UpdateScoreAndText (3, newValue);
+			break;
+			
+			
+		case 4:
+			newValue = (GameInformation.PlayerOneAS.inte + 1) % 19;
+			ASUIUpdater.UpdateScoreAndText (4, newValue);
+			break;
+			
+		case 5:
+			newValue = (GameInformation.PlayerOneAS.wis + 1) % 19;
+			ASUIUpdater.UpdateScoreAndText (5, newValue);
+			break;
+			
+		case 6:
+			newValue = (GameInformation.PlayerOneAS.cha + 1) % 19;
+			ASUIUpdater.UpdateScoreAndText (6, newValue);
+			break;	
+			
+		default: 
+			break;	
 		}
 	}
 }
